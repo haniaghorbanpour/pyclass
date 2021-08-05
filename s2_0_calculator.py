6e45105078f0e3ae99da10b9dbf54cf276551a99
haniaghorbanpour <87777633+haniaghorbanpour@users.noreply.github.com>
2021-08-05 11:17:18 +0430

    Create s2_0_calculator.py

---
+++ s2_0_calculator.py
@@ -0,0 +1,11 @@
+adad1 = int(input())
+adad2 = int(input())
+amalgar = input()
+if amalgar == "+":
+    print (adad1+adad2)
+elif amalgar == "-":
+    print (adad1-adad2)
+elif amalgar == "*":
+    print (adad1*adad2)
+else:
+    print("i dont know")
