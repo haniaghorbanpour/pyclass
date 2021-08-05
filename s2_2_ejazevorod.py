c4a38b18b1dc75a6e2b2a7dbc2fc63dae91dc97b
haniaghorbanpour <87777633+haniaghorbanpour@users.noreply.github.com>
2021-08-05 12:50:01 +0430

    Create s2_2_ejazevorod.py

---
+++ s2_2_ejazevorod.py
@@ -0,0 +1,10 @@
+esm = input("enter name")
+sen = int(input("enter age"))
+if (esm == 'ali' and sen>30) or (sen<20 and esm != ali):
+    vorodi = True
+else:
+    vorodi = False
+if vorodi == True:
+    print("ejaze vorod dari")
+else:
+    print("ejaze vorod nadari")
