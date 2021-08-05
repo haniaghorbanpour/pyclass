bc9ab2bac5ab94d2b162e2578dbf9b31036e5608
haniaghorbanpour <87777633+haniaghorbanpour@users.noreply.github.com>
2021-08-05 12:07:52 +0430

    Create s2_1_mosalas.py

---
+++ s2_1_mosalas.py
@@ -0,0 +1,8 @@
+adad1= int(input())
+adad2= int(input())
+adad3= int(input())
+
+if adad1+adad2>adad3 and adad1+adad3>adad2 and adad2+adad3>adad1:
+    print("azla mosalas hast")
+else:
+    print("ghabele ghabol nist")
