55435b38184ef365b23ddf505f7ec17eba737aef
haniaghorbanpour <87777633+haniaghorbanpour@users.noreply.github.com>
2021-08-08 10:42:39 +0430

    Create s2h_2_bakhshpaziri.py

---
+++ s2h_2_bakhshpaziri.py
@@ -0,0 +1,6 @@
+vorodi1 = int(input())
+vorodi2 = int(input())
+if vorodi1 % vorodi2 == 0:
+    print(f"{vorodi1} bar {vorodi2} bakhshpazir ast")
+else:
+    print(f"{vorodi1} bar {vorodi2} bakhshpazir nist")
