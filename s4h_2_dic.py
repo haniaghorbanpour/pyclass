3038be047bbf23a4998d198d2362fa88f709007c
haniaghorbanpour <87777633+haniaghorbanpour@users.noreply.github.com>
2021-09-01 23:29:41 +0430

    Create s4h_2_dic.py

---
+++ s4h_2_dic.py
@@ -0,0 +1,8 @@
+etelaat = dict()
+etelaat["First name"]= input('Enter your First name: \n')
+etelaat["Last name"]= input('Enter your Last name: \n')
+etelaat["Age"]= input('Enter your Age: \n')
+etelaat["Marital status"]= input('Enter your Marital status: \n')
+
+print(etelaat)
+del etelaat
