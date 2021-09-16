a255d9fc3fea87f795826c28bf07b150f816b1d6
haniaghorbanpour <87777633+haniaghorbanpour@users.noreply.github.com>
2021-09-16 16:43:36 +0430

    Update s6h_5_moratab.py

--- s6h_5_moratab.py
+++ s6h_5_moratab.py
@@ -2,7 +2,6 @@ num=[1,3,7,2,5,4,9,8]
 print(len(num))
 for i in range(0,len(num)):
     for j in range(i+1,len(num)):
-        print(num[i], num[j], max)
         if num[j]>=num[i]:
             max=num[j]
             num[j]=num[i]
